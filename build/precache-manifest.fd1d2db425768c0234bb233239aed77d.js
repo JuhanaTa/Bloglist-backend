00d75818a20dda5c50b5f203ee4bcbc7b2297d65
JuhanaTa <61382156+JuhanaTa@users.noreply.github.com>
2020-07-16 15:52:31 +0300

    first commit

---
+++ build/precache-manifest.fd1d2db425768c0234bb233239aed77d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc17df166b631d5a53fb528956e9de4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60069dc096902c18b174",
+    "url": "/static/css/main.9cab1346.chunk.css"
+  },
+  {
+    "revision": "e63e002692fc51e24a45",
+    "url": "/static/js/2.aac9e4fe.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.aac9e4fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60069dc096902c18b174",
+    "url": "/static/js/main.b0f59c9f.chunk.js"
+  },
+  {
+    "revision": "7eb8d6388ecf90523c80",
+    "url": "/static/js/runtime-main.7dc31f0f.js"
+  }
+]);
